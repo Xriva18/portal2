5e4d34c8a549e31b9de4f367221e956f2d6cf94f
Xriva18 <kevinguanoluisa2004@gmail.com>
2024-07-13 15:31:49 -0500

    Toma de funcion recargar primero

--- js/recargaprimero.js
+++ js/recargaprimero.js
@@ -9,5 +9,20 @@ $(document).ready(function () {
     $('#EspacioOcultar').hide();
     $("#ExcepcionObservacionPrest").hide();
     $('html, body').animate({ scrollTop: $(document).height() - $(window).height() }, 0, 'easeInOutExpo');
+    function recorrerArray() {
+        $('#listEmpleados').empty();
+        for (var i = 0; i < users.length; i++) {
+            var Botones = '<td width="5%" style="text-align:center;">' +
+                '<div class="CeldasTabla" onclick="seleccionarRow(\'' + users[i].id + '\');">Saludar</div></td>';
+            newTr = '<tr class="text-center">' +
+
+                '<td>' + users[i].name + '</td>' +
+                '<td>' + users[i].age + '</td>' +
+                '<td>' + users[i].occupation + '</td>' +
+                Botones +
+                '</tr>';
+            $('#listEmpleados').append(newTr);
+        }
+    }
     return false;
 });
