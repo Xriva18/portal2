45a4ec017702abb3b1387513328b60a30b2bf423
Xriva18 <kevinguanoluisa2004@gmail.com>
2024-07-18 09:31:46 -0500

    Cambios de mantenimiento

--- js/mantenimiento.js
+++ js/mantenimiento.js
@@ -16,7 +16,6 @@ function tipoMantenimiento() {
 
 }
 function GuardarUsuarioMantenimiento() {
-
     users.push({
         "id": users.length + 1,
         "name": $('#txtNombreCliente').val(),
@@ -60,5 +59,53 @@ function recorrerArray() {
             Botones +
             '</tr>';
         $('#listEmpleados').append(newTr);
+    }
+}
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function () {
+    /*if ($('#txtNombreCliente').val() == '') {
+        swal('Ingrese nombre del cliente');
+        return;
+    }
+    if ($('#txtCedula').val() == '') {
+        swal('Ingrese su cédula');
+        return;
+    }
+    if ($('#txtFechaSolicitud').val() == '') {
+        swal('Ingrese la fecha');
+        return;
+    }
+    if ($('#selSurcursal').val() == '0') {
+        swal('Ingrese la sucursal');
+        return;
+    }
+    if ($('txtDireccion').val() == '') {
+        swal('Ingrese la direccion');
+        return;
+    }*/
+
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
     }
 }
